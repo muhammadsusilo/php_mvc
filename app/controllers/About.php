13f017afb41a37d2437efcc834a8567b45077cc0
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-07-09 13:13:45 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,24 @@
 <?php
 
-class About {
+class About extends Controller{
 
-  public function index($name = "Muhammad Susilo", $pekerjaan = "Pengusaha"){
-    echo "Halo, nama saya $name, saya seorang $pekerjaan";
+  public function index($name = "Muhammad Susilo", $pekerjaan = "Pengusaha", $umur = 25)
+  {
+    $data = [
+      'name' => $name,
+      'pekerjaan' => $pekerjaan,
+      'umur' => $umur,
+      'judul' => 'About Me'
+    ];
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
   public function page()
   {
-    echo "About/page";
+    $data['judul']= "Pages";
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,9 @@
+
+  <h3>About Me</h3>
+  <p>Data Pengusaha</p>
+  <ul>
+    <li>Nama :<?= $data['name']; ?></li>
+    <li>Pekerjaan :<?= $data['pekerjaan']; ?></li>
+    <li>Umur :<?= $data['umur']; ?></li>
+    <li><?=$data['judul'];?></li>
+  </ul>
