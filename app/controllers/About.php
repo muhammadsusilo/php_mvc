0c0a4cc504419aa60aec912d8d39b67539d24c82
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-07-09 10:49:33 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+
+  public function index($name = "Muhammad Susilo", $pekerjaan = "Pengusaha"){
+    echo "Halo, nama saya $name, saya seorang $pekerjaan";
+  }
+  public function page()
+  {
+    echo "About/page";
+  }
+}
